2bca6a682ec2299da78d3bc25e182f5b0abd8f7d
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-10-14 20:29:37 +0530

    2nd lec

---
+++ no_002_patterns/no_3_pattern2.java
@@ -0,0 +1,29 @@
+package no_002_patterns;
+
+public class no_3_pattern2 {
+
+    public static void main(String[] args) {
+
+
+//-------- PATTERN 2 
+
+/* 
+ 
+        1 2 3
+        1 2 3 
+        1 2 3 
+
+*/
+        int n = 3;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+            System.out.print(j);
+
+            }
+            System.out.println();
+            
+        
+        }
+    }
+        
+}
