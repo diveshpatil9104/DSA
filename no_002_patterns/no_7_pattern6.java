c7722dd3d54f891b11a4de277e868a635a04f58c
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-10-19 20:10:16 +0530

    2nd lec

---
+++ no_002_patterns/no_7_pattern6.java
@@ -0,0 +1,25 @@
+package no_002_patterns;
+
+public class no_7_pattern6 {
+    public static void main(String[] args) {
+
+
+/*  PATTERN 6
+        1
+        12
+        123
+        1234
+*/
+    int n = 5;
+        for (int line = 1; line <= n; line++) {
+            for (int num = 1; num <= line; num++) {
+            System.out.print(num);
+        
+        }
+        System.out.println();
+    
+    }
+
+
+    }
+}
