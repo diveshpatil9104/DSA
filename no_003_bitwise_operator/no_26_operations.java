af01a3125232116d11b5c5a458f1fb1aafb3c402
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-11-05 10:41:56 +0530

    3rd lec

--- no_003_bitwise_operator/no_26_operations.java
+++ no_003_bitwise_operator/no_26_operations.java
@@ -10,12 +10,12 @@ EG. n = 15   ,  i = 2;
         BNINARY REPRESENTATION OF 15  00001111
 
         i = 2
-        SO iTH BIT MEANS PUT iTH PLACE = 1 AND  OTHERS ARE O 
+        SO GET iTH BIT MEANS PUT iTH PLACE = 1 AND  OTHERS ARE O 
 
                        | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |
                   i =  | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 STEP   : 00001111      i=2  
-----> &  00000100  ----->  1<<i   ()
+----> &  00000100  ----->  1<<i   (2)
         -----------
          0001000 
          ans is ----> 1 (means 1 is present in ith 2 place )
@@ -44,17 +44,18 @@ EG. n = 15   ,  i = 2;
         BNINARY REPRESENTATION OF 15  00001111
 
         i = 2
-        SO iTH BIT MEANS PUT iTH PLACE = 1 AND  OTHERS ARE O 
+        SO SET iTH BIT MEANS SET iTH BIT (SET 0 AS 1 AND 1 AS 1).. 0 HOGA TO 1 KARNEKA AND 1 KA 1 HI RAHEGA KYONKI O ALREDY 1 PE SET HAIN 🙂
+
 
                        | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |
-                  i =  | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+                  i =  | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | 
 STEP   : 00001111      i=2  
-----> |  00000100  ----->  1<<i   ()
+----> |  00000100  ----->  1<<i   (2)
         -----------
          00001111    
          ans is ----> 15
  */
-
+ 
         int n1 = 15;
         int i1 = 2;
         int bitmask1 = 1<<i1;
@@ -63,19 +64,20 @@ STEP   : 00001111      i=2
 
 
 /* ------------------------------------------ CLEAR iTH BIT  ------------------------------------------------------------------------------------------------------------------   
-      3. CLEAR GET iTH BIT 
+      3. CLEAR iTH BIT 
 
 EG. n = 15   ,  i = 2; 
         BNINARY REPRESENTATION OF 15  00001111
 
         i = 2
-        SO iTH BIT MEANS PUT iTH PLACE = 1 AND  OTHERS ARE O 
+        SO CLEAR iTH BIT MEANS CLEAR iTH BIT (SET 0 AS 0 AND 1 AS 0).. 1 HOGA TO 0 KARNEKA AND 0 KA 0 HI RAHEGA KYONKI O ALREDY CLEAR HAIN 🙂
+
 
                        | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |
                   i =  | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 STEP   : 00001111       
          11111011  -----> ~(1<<i)    (i=2 )
-     & -----------
+     & ----------- 
          00001011                                                               
                                                                             
          ans is ----> 11                                                          
@@ -95,14 +97,15 @@ EG. n = 15   ,  i = 2;
         BNINARY REPRESENTATION OF 15  00001111
 
         i = 2
-        SO iTH BIT MEANS PUT iTH PLACE = 1 AND  OTHERS ARE O 
+        SO CLEAR LAST iTH BIT MEANS CLEAR iTH BIT (SET 0 AS 0 AND 1 AS 0 FOR ALL LAST TO iTH).. 1 HOGA TO 0 KARNEKA AND 0 KA 0 HI RAHEGA KYONKI O ALREDY CLEAR HAIN 🙂
+
 
                        | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |
                   i =  | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 STEP   : 00001111       
          00001100  -----> ~(0<<i)    (i=2 )
      & -----------
-         00001100                                                               
+         00001100    // HERE WE CLEAR LAST ALL 2 BIT BCOZ I IS 2                                                            
                                                                             
          ans is ----> 12                                                     
  */ 
