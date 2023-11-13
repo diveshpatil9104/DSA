d6148f07bc1f357aed601b120fa02f4745124751
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-11-14 00:15:25 +0530

    5th lec

--- no_005_arrays/no_33_questions.java
+++ no_005_arrays/no_33_questions.java
@@ -672,6 +672,9 @@ Each pair should be sorted ie the first value should be less than or equals to t
 Return the list of pairs sorted in non-decreasing order of their first value.
 In case if two pairs have the same first value, the pair with a smaller second value should come first.
 
+    FOR EG : sum_array[] = { 1 , 2 , 3  , 4 , 5 , 6 };
+            s = 5
+    OUTPUT :  (1 4) (2 3 )
 */
 
 
@@ -691,7 +694,22 @@ In case if two pairs have the same first value, the pair with a smaller second v
             
          }
 
+/* 
+    QN NO. 25 -----> You are given an array/list ARR consisting of N integers. Your task is to find all the distinct triplets present in the array which adds up to a given number K.
+An array is said to have a triplet {ARR[i], ARRU[j], ARR[k]} with sum = 'K' if there exists three
+indices i, j and k such that i!=j, j!=k and i!=j and ARR[i) + ARR[j] + ARR[k] = 'K'.
+Note:
+1. You can return the list of values in any order. For example, if a valid triplet is {1, 2, -3}, then {2, -3, 1}, {-3, 2, 1) etc is also valid triplet.
+Also, the ordering of different triplets can be random i.e if there are more than one valid triplets, you can return them in any order.
+2. The elements in the array need not be distinct.
+3. If no such triplet is present in the array, then return an empty list, and the output printed for such a test case will be "-1".
 
+    FOR EG : sum_array[] = { 1 , 2 , 3  , 4 , 5 , 6 };
+                s = 12
+    OUTPUT :  ( 3 , 4 , 5 ) (2 , 4  , 6 ) 
+
+
+*/
 
 
 
