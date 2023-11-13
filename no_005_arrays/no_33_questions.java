8e3fa77ccd3a401846a9a020a86e0128a777c759
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-11-13 19:35:46 +0530

    5th lec

--- no_005_arrays/no_33_questions.java
+++ no_005_arrays/no_33_questions.java
@@ -593,7 +593,22 @@ Output: true
            System.out.println(ans1);
 
 
+/*
+    QN NO. 23 -----> You are given two arrays 'A' and 'B' of size 'N' and 'M' respectively. Both these arrays are sorted in non-decreasing order. You have to find the intersection of these two arrays.
+                    Intersection of two arrays is an array that consists of all the common elements occurring in both arrays.
+    Note:
+        1. The length of each array is greater than zero.
+        2. Both the arrays are sorted in non-decreasing order.
+        3. The output should be in the order of elements that occur in the original arrays.
+        4. If there is no intersection present then return a single integer '-1'
 
+
+for eg .    
+           ARRAY1 = { 1 , 2 , 2 , 2 , 3 , 4 }
+           ARRAY2 = { 2 , 2 , 3 , 3 }
+
+           OUTPUT ----> { 2 , 2 , 3 }   (BCOZ THEY ARE COMMON ELEMENT OF ARRAY1 AND ARRAY2)
+ */
 
 
            
