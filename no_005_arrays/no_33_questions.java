39d8f1e1d2cec97ef58b376b96c593fe38fc3eb5
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-11-22 20:07:47 +0530

    5th lec

--- no_005_arrays/no_33_questions.java
+++ no_005_arrays/no_33_questions.java
@@ -698,10 +698,10 @@ Note:
     An array is said to have a triplet {ARR[i], ARRU[j], ARR[k]} with sum = 'K' if there exists three
     indices i, j and k such that i!=j, j!=k and i!=j and ARR[i) + ARR[j] + ARR[k] = 'K'.
     Note:
-1. You can return the list of values in any order. For example, if a valid triplet is {1, 2, -3}, then {2, -3, 1}, {-3, 2, 1) etc is also valid triplet.
-Also, the ordering of different triplets can be random i.e if there are more than one valid triplets, you can return them in any order.
-2. The elements in the array need not be distinct.
-3. If no such triplet is present in the array, then return an empty list, and the output printed for such a test case will be "-1".
+    1.You can return the list of values in any order. For example, if a valid triplet is {1, 2, -3}, then {2, -3, 1}, {-3, 2, 1) etc is also valid triplet.
+        also, the ordering of different triplets can be random i.e if there are more than one valid triplets, you can return them in any order.
+    2. The elements in the array need not be distinct.
+    3. If no such triplet is present in the array, then return an empty list, and the output printed for such a test case will be "-1".
 
     FOR EG : sum_array[] = { 1 , 2 , 3  , 4 , 5 , 6 };
                 s = 12
