7be81b4cf260a3f95a155d8c2305bbfaf76c385c
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-12-16 18:53:06 +0530

    5th lec

--- no_005_arrays/no_33_questions.java
+++ no_005_arrays/no_33_questions.java
@@ -696,7 +696,7 @@ for eg .
     QN NO. 25 -----> You are given an array/list ARR consisting of N integers. Your task is to find all the distinct triplets present in the array which adds up to a given number K.
     An array is said to have a triplet {ARR[i], ARRU[j], ARR[k]} with sum = 'K' if there exists three
     indices i, j and k such that i!=j, j!=k and i!=j and ARR[i) + ARR[j] + ARR[k] = 'K'.
-          Note:
+Note:
     1.You can return the list of values in any order. For example, if a valid triplet is {1, 2, -3}, then {2, -3, 1}, {-3, 2, 1) etc is also valid triplet.
       also, the ordering of different triplets can be random i.e if there are more than one valid triplets, you can return them in any order.
     2. The elements in the array need not be distinct.
