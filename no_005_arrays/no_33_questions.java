43520bee9e9136f47667e1146ec6fd3f09e9df4f
diveshpatil9104 <diveshpatil9104@gmail.com>
2023-12-19 17:56:03 +0530

    5th lec

--- no_005_arrays/no_33_questions.java
+++ no_005_arrays/no_33_questions.java
@@ -693,7 +693,7 @@ for eg .
         }
 
 /* 
-    QN NO. 25 -----> You are given an array/list ARR consisting of N integers. Your task is to find all the distinct triplets present in the array which adds up to a given number K.
+         QN NO. 25 -----> You are given an array/list ARR consisting of N integers. Your task is to find all the distinct triplets present in the array which adds up to a given number K.
     An array is said to have a triplet {ARR[i], ARRU[j], ARR[k]} with sum = 'K' if there exists three
     indices i, j and k such that i!=j, j!=k and i!=j and ARR[i) + ARR[j] + ARR[k] = 'K'.
                 Note:
