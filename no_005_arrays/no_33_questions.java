7aa1b02bec04d8479241f5d96c502e3a129051e0
diveshpatil9104 <diveshpatil9104@gmail.com>
2024-05-11 22:02:50 +0530

    5th lec

--- no_005_arrays/no_33_questions.java
+++ no_005_arrays/no_33_questions.java
@@ -41,7 +41,7 @@ public class no_33_questions {
             }
 
 
-//  ---->> function for QN NO.21 (optimize  scode) :
+//  ---->> function for QN NO.21 (optimize  s  code) :
 
     public static boolean sorted(int matrix3[][], int key2) {
 
